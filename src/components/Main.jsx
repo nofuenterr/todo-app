bda8bdc96283c7e544288a7c2735c2b8312b2740
RR <rrn091301@gmail.com>
2025-12-16 17:05:45 +0800

    feat: add text styles and replace toggle group with radio group for filters

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,43 +3,56 @@ import { useTodoStore } from '../stores/todoStore'
 import { Checkbox } from "radix-ui";
 import { CheckIcon, Cross1Icon } from "@radix-ui/react-icons";
 import { AccessibleIcon } from "radix-ui";
-import { ToggleGroup } from "radix-ui";
+import { RadioGroup } from "radix-ui";
 
 export default function Main() {
   const todoIds = useTodoStore(s => s.todoIds)
   const deleteCompletedTodos = useTodoStore(s => s.deleteCompletedTodos)
 
   return (
-    <main className='grid gap-4 overflow-hidden drop-shadow-md'> 
+    <main className='grid gap-4 overflow-hidden drop-shadow-md text-lg'> 
       <AddTodo />
 
       {todoIds.length !== 0
         ? (
-          <div className='bg-white dark:bg-navy-900 rounded-sm overflow-scroll'>
-            <ul className='text-navy-850 dark:text-purple-100'>
-              {todoIds.map(id => {
-                return <TodoItem key={id} id={id} />
-              })}
-            </ul>
-            <div className='text-gray-600 dark:text-purple-600 py-4 px-5 flex justify-between items-center'>
-              <span aria-label='Number of todos'>{todoIds.length} items</span>
-              <div className='hidden sm:inline-block bg-white dark:bg-navy-900 text-gray-600 dark:text-purple-600'>
-                <FilterGroup />
+          <>
+            <div className='bg-white dark:bg-navy-900 rounded-sm overflow-scroll'>
+              <ul className='text-navy-850 dark:text-purple-100'>
+                {todoIds.map(id => {
+                  return <TodoItem key={id} id={id} />
+                })}
+              </ul>
+              <div className='text-gray-600 dark:text-purple-600 py-4 px-5 flex justify-between items-center text-sm'>
+                <span aria-label='Number of todos'>{todoIds.length} items</span>
+                <div className='hidden sm:inline-block bg-white dark:bg-navy-900'>
+                  <FilterGroup />
+                </div>
+                <button 
+                  className='cursor-pointer hover:text-navy-850 hover:dark:text-purple-300'
+                  onClick={deleteCompletedTodos}
+                  aria-label='Clear completed todos'>
+                    Clear Completed
+                  </button>
               </div>
-              <button className='cursor-pointer' onClick={deleteCompletedTodos} aria-label='Clear completed todos'>Clear Completed</button>
             </div>
-          </div>
+
+            <div className='sm:hidden bg-white dark:bg-navy-900 py-4 grid place-content-center rounded-sm drop-shadow-md'>
+              <FilterGroup />
+            </div>
+          </>
         )
         : (
-          <div className='bg-white dark:bg-navy-900 text-navy-850 dark:text-purple-100 py-4 px-5 grid place-content-center rounded-sm drop-shadow-md'>
-            <p>You currently have no todos.</p>
-          </div>
+          <>
+            <div className='bg-white dark:bg-navy-900 text-navy-850 dark:text-purple-100 py-4 px-5 grid place-content-center rounded-sm drop-shadow-md'>
+              <p>You currently have no todos.</p>
+            </div>
+            <div className='bg-white dark:bg-navy-900 py-4 grid place-content-center rounded-sm drop-shadow-md'>
+              <FilterGroup />
+            </div>
+          </>
         )
       }
 
-      <div className='sm:hidden bg-white dark:bg-navy-900 text-gray-600 dark:text-purple-600 py-4 grid place-content-center rounded-sm drop-shadow-md'>
-        <FilterGroup />
-      </div>
     </main>
   )
 }
@@ -83,7 +96,7 @@ function TodoItem({ id }) {
       <Checkbox.Root
         onClick={() => toggleTodo(id)}
         checked={todo.complete}
-        className={'size-5 rounded-full border-2 border-purple-300 dark:border-purple-800 grid place-content-center cursor-pointer ' + (
+        className={'size-5 rounded-full border-2 border-purple-300 dark:border-purple-800 grid place-content-center cursor-pointer hover:border-gradient-1-end ' + (
           todo.complete ? 'bg-linear-to-r from-gradient-1-start to-gradient-1-end border-none text-white stroke-white' : 'bg-transparent'
         )}
       >
@@ -107,33 +120,27 @@ function FilterGroup() {
   const filterTodoIds = useTodoStore(s => s.filterTodoIds)
 
   return (
-    <ToggleGroup.Root
-      className='flex items-center gap-4 *:cursor-pointer *:aria-checked:text-blue-500'
-      type="single"
+    <RadioGroup.Root
       defaultValue="all"
       aria-label="Filter todos"
+      className='flex items-center gap-4 **:cursor-pointer **:aria-checked:text-blue-500 
+    text-gray-600 dark:text-purple-600  *:hover:text-navy-850 *:hover:dark:text-purple-300 font-bold'
     >
-      <ToggleGroup.Item
-        value="all"
-        aria-label="All todos"
-        onClick={() => filterTodoIds('all')}
-      >
-        <span>All</span>
-      </ToggleGroup.Item>
-      <ToggleGroup.Item
-        value="active"
-        aria-label="Active todos"
-        onClick={() => filterTodoIds('active')}
-      >
-        <span>Active</span>
-      </ToggleGroup.Item>
-      <ToggleGroup.Item
-        value="completed"
-        aria-label="Completed todos"
-        onClick={() => filterTodoIds('completed')}
-      >
-        <span>Completed</span>
-      </ToggleGroup.Item>
-    </ToggleGroup.Root>
+      <div>
+        <RadioGroup.Item className='appearance-none' value="all" onClick={() => filterTodoIds('all')} aria-label="All todos">
+          All
+        </RadioGroup.Item>
+      </div>
+      <div>
+        <RadioGroup.Item className='appearance-none' value="active" onClick={() => filterTodoIds('active')} aria-label="Active todos">
+          Active
+        </RadioGroup.Item>
+      </div>
+      <div>
+        <RadioGroup.Item className='appearance-none' value="completed" onClick={() => filterTodoIds('completed')} aria-label="Completed todos">
+          Completed
+        </RadioGroup.Item>
+      </div>
+    </RadioGroup.Root>
   )
 }
